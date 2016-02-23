6d18c832b22572a2ad5f4a6c194ce86fe5687272
Mykola <mykola.kurylets@gmail.com>
2016-02-23 22:19:28 +0200

    add testing comment

--- app/src/main/java/com/kurylets/mykola/gittest2/MainActivity.java
+++ app/src/main/java/com/kurylets/mykola/gittest2/MainActivity.java
@@ -10,7 +10,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    // test push
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
